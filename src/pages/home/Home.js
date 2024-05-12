d6b274a16b0cf2681529e857b3591fa862456a96
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-13 00:57:57 +0700

    PropertyList,FeaturedProperties,MailList,Footer

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,10 @@ import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import "./Home.css"
 import Featured from '../../components/featured/Featured'
+import PropertyList from '../../components/propertyList/PropertyList'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
+import MailList from '../../components/MailList/MailList'
+import Footer from '../../components/Footer/Footer'
 const Home = () => {
     return (
         <div>
@@ -10,6 +14,12 @@ const Home = () => {
             <Header />
             <div className="homeContainer">
                 <Featured />
+                <h1 className="homeTitle">Browse by property type</h1>
+                <PropertyList />
+                <h1 className="homeTitle1">Homes guests love</h1>
+                <FeaturedProperties />
+                <MailList />
+                <Footer />
             </div>
         </div>
     )
